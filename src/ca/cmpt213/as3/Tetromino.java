28345a5f2d4fc4b457267b33650af0d2ee66e504
steventktran <steventktran@gmail.com>
2018-02-13 15:54:04 -0800

    Branch to be merged when I feel like it

--- src/ca/cmpt213/as3/Tetromino.java
+++ src/ca/cmpt213/as3/Tetromino.java
@@ -2,4 +2,5 @@ package ca.cmpt213.as3;
 
 public class Tetromino {
 
+
 }
--- src/ca/cmpt213/as3/Unit.java
+++ src/ca/cmpt213/as3/Unit.java
@@ -1,5 +1,23 @@
 package ca.cmpt213.as3;
 
 public class Unit {
+    private boolean isOccupied;
+    private boolean isVisibile;
 
+    public Unit(boolean isOccupied, boolean isVisible) {
+        this.isOccupied = isOccupied;
+        this.isVisibile = isVisible;
+    }
+
+    public void reveal() {
+        isVisibile = true;
+    }
+
+    public boolean getOccupier() {
+        return isOccupied;
+    }
+
+    public boolean getVisibility() {
+        return isVisibile;
+    }
 }
