5c9966e9b9a2b37268a73531173cdb1d5344919e
steventktran <steventktran@gmail.com>
2018-02-13 17:57:02 -0800

    Began work on Tank generation

--- src/ca/cmpt213/as3/Tank.java
+++ src/ca/cmpt213/as3/Tank.java
@@ -1,22 +1,54 @@
 package ca.cmpt213.as3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tank {
     private Tetromino tank;
     private int health;
     private int damage;
     private boolean isDestroyed;
 
-    public Tank() {
-        health = 4;
-        damage = 20;
-        isDestroyed = false;
+    private static final int DAMAGE_DROPOFF_FACTOR = 4;
+
+    public Tank(Unit[][] board, int x, int y) {
+      health = 4;
+      damage = 20;
+      isDestroyed = false;
+      tetromino = new Unit[4];
+
+
+    }
+    public void createTank(Unit[][] board, int x, int y) {
+      List possibleSpaces = new ArrayList<Unit>();
+      if( + 1 <= board.size) {
+
+      int numUnits = 0;
+      int currentX = x;
+      int currentY = y;
+      while(numUnits < 4) {
+        tetromino[numUnits] = board[currentX][currentY];
+        board[currentX][currentY].setOccupied();
+        if(currentX + 1 <= board.size) {
+          possibleSpaces.add(board[currentX + 1][currentY]);
+        if(currentX - 1 >= 0) {
+          possibleSpaces.add(board[currentX - 1][currentY]);
+        }
+        if(currentY + 1 <= board[0].size) {
+          possibleSpaces.add(board[currentX][currentY + 1]);
+        }
+        if(currentY - 1 >= 0) {
+          possibleSpaces.add(board[currentX][currentY - 1]);
+        }
+      }
     }
 
     public void takeDamage() {
-        //takeDamage:
-        //if health is greater than 0, health--
-        //depending on health, reduce damage
-        //if health is 0, isDestroyed is true
+        health--;
+        damage = (int) Math.ceil(damage/DAMAGE_DROPOFF_FACTOR); // 20/4 = 5, 5/4 = 2, 2/4 = 1
+        if(health <= 0) {
+          isDestroyed = true;
+        }
     }
 
     public int dealDamage() {
