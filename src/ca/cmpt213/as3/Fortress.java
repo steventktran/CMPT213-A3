0ba6bb17bfca5240b0d0c0fd9506b4f95b333617
steventktran <steventktran@gmail.com>
2018-02-13 18:57:51 -0800

    Added isDestroyed field for Fortress and updated methods accordingly

--- src/ca/cmpt213/as3/Fortress.java
+++ src/ca/cmpt213/as3/Fortress.java
@@ -2,9 +2,11 @@ package ca.cmpt213.as3;
 
 public class Fortress {
     private int health;
+    private boolean isDestroyed;
 
     public Fortress() {
         health = 1500;
+        isDestroyed = false;
     }
 
     public int getHealth() {
@@ -25,14 +27,14 @@ public class Fortress {
     public void takeDamage(int damage) {
         //if health is greater than 0, health - damage;
         //if resulting health is less than 0, just set to 0 and fortress is destroyed.
+        health -= damage;
+        if (health <= 0) {
+          isDestroyed = true;
+        }
     }
 
     //function here that returns true if fortress is destroyed.
     public boolean isFortressDestroyed() {
-        if (health == 0) {
-            return true;
-        }
-
-        return false;
+        return isDestroyed;
     }
 }
